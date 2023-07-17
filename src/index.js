a7e1edfdcf903eb7d91bb635a8340063f8be4728
steve-shim <steveshim496@gmail.com>
2023-07-17 04:36:50 +0000

    Add Props

--- src/index.js
+++ src/index.js
@@ -3,35 +3,31 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+const title = "메타인지의 힘";
+const author = "Amelia Hepworth";
+const img = "https://image.yes24.com/goods/74602725/L";
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job="developer" />
+      <Book title="ramdom title" number={22} />
     </section>
   );
 };
 
-const Book = () => (
-  <article className="book">
-    <Image></Image>
-    <Title />
-    <Author />
-  </article>
-);
-
-const Image = () => (
-  <img src="https://image.yes24.com/goods/74602725/L" alt="" />
-);
-
-const Title = () => <h1>메타인지의 힘</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Amelia Hepworth
-  </h4>
-);
+const Book = (props) => {
+  console.log("props", props);
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+    </article>
+  );
+};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
 
