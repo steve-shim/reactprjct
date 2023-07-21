857c79089b189954f443688f684c2b6377be8303
steve-shim <steveshim496@gmail.com>
2023-07-21 01:53:39 +0000

    Add key props

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     img: "https://image.yes24.com/goods/74602725/L",
     title: "메타인지의 힘",
     author: "Amelia Hepworth",
   },
   {
+    id: 2,
     img: "https://image.yes24.com/goods/119812251/XL",
     title: "울게되는 한국사",
     author: "김재원",
@@ -26,21 +28,16 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return (
-          <div>
-            <h3>{title}</h3>
-            <h6>{author}</h6>
-          </div>
-        );
+        //const { img, title, author } = book;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
 };
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
   // setting props directly
-  //const { img, title, author } = props; // using deconstruction
+  const { img, title, author, children } = props.book; // using deconstruction
   return (
     <article className="book">
       <img src={img} width={250} height={250} alt="" />
